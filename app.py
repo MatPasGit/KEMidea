d1512b2323c4a95d321695aad991a99f440865d8
matpas <matpas@matpas.home>
2020-04-25 23:44:06 +0200

    next2"

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, url_for ,request, redirect
-from flask_sqlalchemy import SQLAlchemy
 import re
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kem'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////../static/ideas.sql'
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kemidea/mysite/static/static/ideas.sql'
+#db = SQLAlchemy(app)
 
 regex = re.compile("p(\s)?a(\s)?s(\s)?i(\s)?e(\s)?r(\s)?b\s+c(\s)?h(\s)?u(\s)?j(\s)?",re.IGNORECASE)
 regex_empty = re.compile("/s+")
-class DBModel(db.Model):
-        __tablename__= 'IDEAS'
-        id = db.Column('id', db.Integer, primary_key=True)
-        idea = db.Column('idea', db.Unicode)
+#class DBModel(db.Model):
+#        __tablename__= 'IDEAS'
+#        id = db.Column('id', db.Integer, primary_key=True)
+#        idea = db.Column('idea', db.Unicode)
 
-        def __init__ (self,idea):
-                self.idea = idea
+#        def __init__ (self,idea):
+#                self.idea = idea
 
 
 #ex = DBModel("witam")
@@ -31,21 +31,25 @@ def check_if_exist(input, db_list):
 
 @app.route('/', methods= ["POST","GET"])
 def hello_world():
-    db_ideas = DBModel.query.all()
-    idea_count = 0
-    ideas_list = []
-    for ids in db_ideas:
-        idea_count += 1
-        ideas_list.append(ids.idea)
+    conn=sqlite3.connect('home/kemidea/mysite/static/ideas.sql')
+    cursor=conn.cursor()
+    cursor.execute("SELECT idea FROM IDEAS")
+    ideas_list=cursor.fetchone()
+    cursor.execute("SELECT COUNT(idea) FROM IDEAS")
+    idea_count=cursor.fetchone()
+    conn.close()
+    cursor.close()
 
     if request.method == "POST":
         idea = request.form["POMYSL"]
         if idea=="" or check_if_exist(idea, ideas_list) or regex_empty.match(idea) or idea.casefold()=="pasierb chuj"or regex.match(idea):
             return redirect(url_for("error"))
         else:
-            obj = DBModel(idea)
-            db.session.add(obj)
-            db.session.commit()
+            connn=sqlite3.connect('home/kemidea/mysite/static/ideas.sql')
+            cursorn=connn.cursor()
+            cursorn.execute("INSERT INTO IDEAS(idea) VALUES (" + idea + ")")
+            connn.close()
+            cursorn.close()
             return redirect(url_for("sql_added"))
     else:
         return render_template("index.html", idea_count=idea_count, ideas_list=ideas_list)
