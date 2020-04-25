f7ea13f781ab8c229da4f92f84b8a698b1deac48
matpas <matpas@matpas.home>
2020-04-26 01:05:14 +0200

    next2" ; "

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for ,request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 import re
-import sqlite3
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'kem'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kemidea/mysite/static/static/ideas.sql'
-#db = SQLAlchemy(app)
+#app.config['SECRET_KEY'] = 'kem'
+#app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////../static/ideas.sql'
+
+app.config['MYSQL_DATBASE_HOST'] = 'kemidea.mysql.pythonanywhere-services.com'
+app.config['MYSQL_DATABASE_USER'] = 'kemidea'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Behaviour2@'
+app.config['MYSQL_DATABASE_DB'] = 'kemidea$KEM'
+mysql = MySQL(app)
+
+
 
 regex = re.compile("p(\s)?a(\s)?s(\s)?i(\s)?e(\s)?r(\s)?b\s+c(\s)?h(\s)?u(\s)?j(\s)?",re.IGNORECASE)
 regex_empty = re.compile("/s+")
@@ -31,25 +39,28 @@ def check_if_exist(input, db_list):
 
 @app.route('/', methods= ["POST","GET"])
 def hello_world():
-    conn=sqlite3.connect('../static/ideas.sql')
-    cursor=conn.cursor()
-    cursor.execute("SELECT idea FROM IDEAS")
-    ideas_list=cursor.fetchone()
-    cursor.execute("SELECT COUNT(idea) FROM IDEAS")
-    idea_count=cursor.fetchone()
-    conn.close()
-    cursor.close()
+    curr = mysql.connection.cursor()
+    curr.execute("SELECT idea FROM IDEAS")
+    mysql.connection.commit()
+    ideas_list = curr.fetchall()
+    curr.execute("SELECT COUNT(idea) FROM IDEAS")
+    mysql.connection.commit()
+    idea_count = curr.fetchall()
+    curr.close()
+
+#    for ids in db_ideas:
+ #       idea_count += 1
+ #       ideas_list.append(ids.idea)
 
     if request.method == "POST":
         idea = request.form["POMYSL"]
         if idea=="" or check_if_exist(idea, ideas_list) or regex_empty.match(idea) or idea.casefold()=="pasierb chuj"or regex.match(idea):
             return redirect(url_for("error"))
         else:
-            connn=sqlite3.connect('../static/ideas.sql')
-            cursorn=connn.cursor()
-            cursorn.execute("INSERT INTO IDEAS(idea) VALUES (" + idea + ")")
-            connn.close()
-            cursorn.close()
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO IDEAS(idea) VALUES (%s)", (idea))
+            mysql.connection.commit()
+            cur.close()
             return redirect(url_for("sql_added"))
     else:
         return render_template("index.html", idea_count=idea_count, ideas_list=ideas_list)
@@ -64,3 +75,4 @@ def sql_added():
 
 if __name__ == '__main__':
     app.run()
+
